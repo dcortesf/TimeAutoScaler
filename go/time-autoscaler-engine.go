5b7a68eaaffaabfff86d52abee0d53144b027503
xIS11103 <dcortesf@servexternos.isban.es>
2016-09-09 13:33:27 +0200

    Beta version

--- go/time-autoscaler-engine.go
+++ go/time-autoscaler-engine.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"crypto/tls"
+	"encoding/json"
+	"bytes"
 	"fmt"
 	"time"
 	"log"
@@ -9,12 +12,18 @@ import (
 	"os"
 	//flag "github.com/spf13/pflag"
 	"github.com/spf13/viper"
+	"net/http"
+	"strconv"
 )
 
+//Data structure which contains the
 type TimeRule struct {
-        Id string
-        DeploymentConfig string
-				Scale int
+	Region          string            `json:"region" yaml:"region"`
+	Project         string            `json:"project" yaml:"project"`
+	Dc              string            `json:"dc" yaml:"dc"` //alias deploymentconfig
+	TimeExpression  string            `json:"timeExpression" yaml:"timeExpression"`
+	Alias						string						`json:"alias" yaml:"alias"`
+	Instances       int               `json:"instances" yaml:"instances"`
 }
 
 func mainLoop() {
@@ -100,34 +109,103 @@ func mainLoop() {
 		fmt.Println("expr2: ",expr2)
 		fmt.Println("expr3: ",expr3)
 		fmt.Println("expr4: ",expr4)
-		fmt.Println(len(expr1))
 
+		//Check expressions in database
 
+		//1.
 		err = c.Find(bson.M{"timeExpression": expr1}).All(&cResult)
 		if err != nil {
 						log.Fatal(err)
 		}
 
-		fmt.Println(len(cResult))
-
 		for _, item := range cResult {
-        fmt.Printf(" DeploymentConfig: %s - scale: %d \n", item.DeploymentConfig, item.Scale)
+        fmt.Printf(" DeploymentConfig: %s - scale: %d \n", item.Dc, item.Instances)
+				scale(item)
     }
 
-		////////
-		/*err = cc.Find(bson.M{"id": hourNow}).All(&cResult)
+		//2.
+		err = c.Find(bson.M{"timeExpression": expr2}).All(&cResult)
 		if err != nil {
-				log.Fatal(err)
+						log.Fatal(err)
 		}
 
-		fmt.Println(len(cResult))
+		for _, item := range cResult {
+        fmt.Printf(" DeploymentConfig: %s - scale: %d \n", item.Dc, item.Instances)
+				scale(item)
+    }
+
+		//3.
+		err = c.Find(bson.M{"timeExpression": expr3}).All(&cResult)
+		if err != nil {
+						log.Fatal(err)
+		}
 
 		for _, item := range cResult {
-			fmt.Printf(" DeploymentConfig: %s - scale: %d \n", item.DeploymentConfig, item.Scale)
-		}*/
+        fmt.Printf(" DeploymentConfig: %s - scale: %d \n", item.Dc, item.Instances)
+				scale(item)
+    }
+
+		//4.
+		err = c.Find(bson.M{"timeExpression": expr4}).All(&cResult)
+		if err != nil {
+						log.Fatal(err)
+		}
+
+		for _, item := range cResult {
+        fmt.Printf(" DeploymentConfig: %s - scale: %d \n", item.Dc, item.Instances)
+				scale(item)
+    }
+
+
 	}
 }
 
 func main() {
    mainLoop()
+}
+
+func scale(data TimeRule) bool{
+		fmt.Printf("Scaling DeploymentConfig %s in namespace %s in region %s", data.Dc, data.Project, data.Region)
+
+		ioJsonData := new(bytes.Buffer)
+    json.NewEncoder(ioJsonData).Encode(data)
+
+
+		transport := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+
+	scaleUrl := "https://gpc-api-service-globalpaas-dev.appls.boaw.paas.gsnetcloud.corp/api/ose/scale/"+data.Region+"/"+data.Project+"/"+data.Dc+"/"+strconv.Itoa(data.Instances)
+	fmt.Println(scaleUrl)
+
+	client := &http.Client{Transport: transport}
+	req, _ := http.NewRequest("GET", scaleUrl, ioJsonData)
+
+	req.Header.Add("Authorization", "eyJ0b2tlbiI6eyJjYXAxIjoiVzZuQVpqRnVkQlJfQ3hmN1BRTzV5a0pRVmpfLTl4QXdnOVZwR2lDdzN2SSIsImN0bzIiOiJzRkJpWHJSSlF1U2xTY2xHaFhWV1NYbXJuY05kQXVOaWdTWE9EV3NlYzBnIiwiYm9hdyI6ImxRN1NhUEZPMEtISjBnajRGd1Atak9aMWxaWVFBNFl5WS1yekZ4S2JoSnMiLCJib2FlIjoiTGhweEdHN2lFYnBvUG52cjRobVZMOW9wMlEtLVJOb2lSYXdBYmhKaGIxSSJ9fQ==")
+	req.Header.Add("Content-Type", "application/json")
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("Unknow error scaling dc %s, in namespace %s in region %s\n",data.Dc,data.Project,data.Region)
+		fmt.Println(err)
+	} else {
+		if res.StatusCode == 200 {
+			fmt.Println("Time-rule has been added successfully")
+			return true
+		}else{
+			switch res.StatusCode {
+	    	case 403:
+					fmt.Printf("You're not allowed in project %s\n",data.Project)
+	        return false
+				case 404:
+					fmt.Printf("No rules are found for selected DC %s\n",data.Project)
+	        return false
+				default:
+					fmt.Println("Error: %d\n",res.StatusCode)
+					return false
+	    	}
+		}
+
+	}
+
+	return true
 }
